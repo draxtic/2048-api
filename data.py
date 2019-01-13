3c9faabf721dbe106d03b3f573141cc5109b38e7
draxtic <draxtic@sjtu.edu.cn>
2019-01-13 11:35:23 +0800

    Add files via upload

--- data.py
+++ data.py
@@ -1,24 +1,14 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 from game2048.game import Game
 from game2048.agents import Agent, RandomAgent, ExpectiMaxAgent
-
-
-# In[2]:
-
 
 import numpy as np
 import datetime
 import csv
 import math
 
-class DataAgent(ExpectiMaxAgent):
+class Data(ExpectiMaxAgent):
     
-    def auto_log(self, data_dir="./data/", max_iter=np.inf):
+    def log(self, data_dir="./data/", max_iter=np.inf):
         
         filename = data_dir + datetime.datetime.now().strftime('%y%m%d_%H%M%S_%f') + ".csv"
         with open(filename,"w") as csvfile: 
@@ -34,43 +24,29 @@ class DataAgent(ExpectiMaxAgent):
                 self.game.move(direction)
                 n_iter += 1
         DataAgent.count += 1
-        print("#%d\t"%DataAgent.count, "文件保存到：",filename,"数据量：",n_iter)
+Data.count = 0
 
-DataAgent.count = 0
-
-
-# In[3]:
-
-
-def gen(data_dir = "./multi/data", number = 10):
+def gen(data_dir = "./m/data", number = 10):
     for i in range(number):
         game = Game(4, score_to_win=2048, random=False)
         agent = DataAgent(game, display=None)
         agent.auto_log(data_dir=data_dir)
 
-
-# In[4]:
-
-
 THREAD_NUM = 8
 LOGFILE_NUM = 500
 
 import os
-if not os.path.exists("./multi/"):
-    os.mkdir("./multi/")
+if not os.path.exists("./m/"):
+    os.mkdir("./m/")
 for i in range(THREAD_NUM):
-    path = "./multi/data%d/"%i
+    path = "./m/data%d/"%i
     if not os.path.exists(path):
         os.mkdir(path)
-
-
-# In[5]:
-
 
 import _thread
 DataAgent.number = 0
 for i in range(THREAD_NUM):
-    _thread.start_new_thread( gen, ("./multi/data%d/"%i, LOGFILE_NUM) )
+    _thread.start_new_thread( gen, ("./m/data%d/"%i, LOGFILE_NUM) )
 
 
 while 1:
